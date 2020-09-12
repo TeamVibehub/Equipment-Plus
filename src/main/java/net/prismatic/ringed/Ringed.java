0da2ddfc28604caac92ffc2a1d95b3f14d81bf59
PrismaticYT <realprismaticyt@gmail.com>
2020-09-13 07:01:48 +1200

    Set up devenv

---
+++ src/main/java/net/prismatic/ringed/Ringed.java
@@ -0,0 +1,10 @@
+package net.prismatic.ringed;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ringed implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
